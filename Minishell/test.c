42900aea7a504fb125cae7d58e0cd05839fa3af2
JaviBarrio6 <javicraft1998@gmail.com>
2020-06-19 21:58:45 +0200

    Practica

--- Minishell/test.c
+++ Minishell/test.c
@@ -1,27 +1,76 @@
 // Para compilar gcc -ansi -g -Wall -Wextra -static test.c libparser_64.a -o test -std=c99
 // o también gcc  test.c libparser_64.a -static -o test
 
+#include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
 #include <unistd.h>
+
 
 #include "parser.h"
 
 //Sección de Variables Globales
 	
 	tline * line; //Variable de tipo line
+	const char changeDirectory[] = "cd";
 	
 // Fin Sección de Variables Globales
 
 void
-closePipes(int p[2]){
+executeCd (tline *line){
+	//Sección de Variables Locales
 	
-	close(p[0]); //Cerramos el descriptor de lectura de fichero del pipe
-	close(p[1]); //Cerramos el descriptor de escritura de fichero del pipe
+	char *home;
+	int error;
 	
+	//Fin de la Sección de Variables
+	
+	if ((line->commands[0].argc) == 1){ //Si solo han introducido cd, un argumento
+		
+		home = getenv("HOME"); //Capturamos la variable del entorno HOME
+		
+		error = chdir(home); //Cambiamos el directorio a home
+		if (error < 0){ //Si ese directorio no existe
+			fprintf(stderr, "%s No existe \n", home);
+		}	
+	} else { //Si nos dan el nombre del directorio al que queremos ir
+		
+		error = chdir(line->commands[0].argv[1]); //Cambiamos el directoria al que nos dan
+		if (error < 0){ //Si ese directorio no existe
+			fprintf(stderr, "%s No existe \n", line->commands[0].argv[1]);
+		}		
+	}
+	
+}
+
+bool
+isChangeDirectory(tline *line){
+	
+	if (strcmp(line->commands[0].argv[0], changeDirectory) == 0){ //Si introducimos cd como argumento
+		
+		return true; //Devolvemos TRUE
+		
+	} else {
+		
+		return false; //Si no, FALSE
+		
+	}
+		
+}
+
+void
+closePipes(int **p){
+	
+	for (int i = 0; i < (line->ncommands-1); i++){
+		
+		close(p[i][0]); //Cerramos el descriptor de lectura de fichero del pipe
+		close(p[i][1]); //Cerramos el descriptor de escritura de fichero del pipe
+	}	
 }
 
 void
@@ -29,17 +78,23 @@ executeLine(void){
 	//Sección de Variables Locales
 	
 	int i; //Variable contadora
-	int p[2][2];
-	pid_t PID; //Variable para guardar el PID del hijo
-	
+	int PID; //Variable para guardar el PID del hijo
+	int **p; //Variable para las pipes mediante memoria dinámica
+	int fd;
 	
 	//Fin de la Sección de Variables
 	
-	for (i = 0; i < line->ncommands; i++){ //Bucle de recorrido del número de comandos
-		pipe(p[i]); //Abrimos dos descriptores de fichero por cada comando introducido 
+	p= malloc(sizeof(int*)*line->ncommands-1); 
+	//Reservamos la memoria de un entero tantas veces como número de mandatos - 1
+	for (i = 0; i < (line->ncommands-1); i++){ //Bucle de recorrido del número de mandatos
+		p[i] = malloc(sizeof(int)*2); //Reservamos la memoria que ocupan dos enteros
 	}
 	
-	for (i = 0; i < line->ncommands; i++){ //Bucle de recorrido del número de comandos
+	for (i = 0; i < (line->ncommands-1); i++){ //Bucle de recorrido del número de mandatos
+		pipe(p[i]); //Abrimos dos descriptores de fichero por cada comando introducido
+	}
+	
+	for (i = 0; i < line->ncommands; i++){ //Bucle de recorrido del número de mandatos
 		
 		PID = fork(); //Creamos un proceso hijo
 		
@@ -48,7 +103,62 @@ executeLine(void){
 			fprintf(stderr, "Error al crear proceso hijo"); //Notificación del error por la salida de error
 			exit(1); //A partir de ahora el valor de error 1, se utilizará para el error al crear un proceso hijo
 			
-		} else if (PID == 0){ //Proceso Hijo
+		} 
+		
+		if (PID == 0){ //Proceso Hijo
+			
+			//Redirecciones
+			
+			if ((i == 0) && (line->redirect_input)){ //Si es el primer mandato y tiene redirección de entrada
+				
+				fd = open(line->redirect_input, O_RDONLY); //Abrimos el fichero
+				
+				if (fd < 0){
+					fprintf(stderr, "Error al abrir fichero"); //Notificación del error por la salida de error
+					exit(3); //A partir de ahora el valor de error 3, se utilizará para el error al abrir un fichero
+				} else {
+					
+					dup2(fd, 0); //Nuestra entrada estandar ahora es el fichero
+					close(fd); //Cerramos el fichero
+					
+				}
+				
+			}
+			
+			if ((i == line->ncommands-1) && (line->redirect_output)){ //Si es el último y tiene redirección de entrada
+				
+				fd = creat(line->redirect_output, 0644); //Creamos el fichero
+				
+				if (fd < 0){
+					fprintf(stderr, "Error al crear fichero"); //Notificación del error por la salida de error
+					exit(4); //A partir de ahora el valor de error 4, se utilizará para el error al crear un fichero
+				} else {
+					
+					dup2(fd, 1); //Nuestra salida estandar ahora es el fichero
+					close(fd); //Cerramos el fichero
+					
+				}
+				
+			}
+			
+			if ((i == line->ncommands-1) && (line->redirect_error)){ //Si tiene redirección de Error
+				
+				fd = creat(line->redirect_error, 0644); //Creamos el fichero
+				
+				if (fd < 0){
+					fprintf(stderr, "Error al crear fichero"); //Notificación del error por la salida de error
+					exit(4); //A partir de ahora el valor de error 4, se utilizará para el error al crear un fichero
+				} else {
+					
+					dup2(fd, 2); //Nuestra salida de error ahora es el fichero
+					close(fd); //Cerramos el fichero
+					
+				}
+				
+			}
+			
+			
+			//Fin de Redirecciones
 			
 			if (line->ncommands > 1){ //Si el numero de comandos es > 1
 				
@@ -56,18 +166,20 @@ executeLine(void){
 					
 					dup2(p[i][1], 1); //Nuestra salida estándar sera el pipe
 					
-				} else if (i == (line->ncommands -1)){ //Si el mandato es el último
+					
+				} else if (i == (line->ncommands-1)){ //Si el mandato es el último
 					
 					dup2(p[i-1][0], 0); //Nuestra entrada estándar sera el pipe donde ha escrito el anterior
+					
 					
 				} else { //Si el mandato está entre el primero y el ultimo
 					
 					dup2(p[i-1][0], 0); //Nuestra entrada estándar sera el pipe donde ha escrito el anterior
-					dup2(p[i][1], 1); //Nuestra salida estándar sera el pipe
+					dup2(p[i][1], 1); //Nuestra salida estándar sera el pipe*/
 					
 				}
 				
-				closePipes(p[i]);
+				closePipes(p);
 			}
 			
 			execvp(line->commands[i].filename, line->commands[i].argv); //Ejecutamos el mandato i
@@ -76,9 +188,18 @@ executeLine(void){
 		}
 	}
 	
-	for (i = 0; i < line->ncommands; i++){
+	closePipes(p); //Cerramos los pipes
+	
+	for (i = 0; i < (line->ncommands-1); i++){ //Bucle de recorrido del número de mandatos
+		free(p[i]); //Liberamos la memoria
+	}
+	
+	free(p); //Liberamos memoria
+			
+	for (i = 0; i < line->ncommands; i++){ //Esperamos a los hijos
 		wait(NULL);
 	}
+	
 }
 
 int
@@ -86,17 +207,35 @@ main(void) {
 	//Sección de Variables Locales
 	
 	char buffer[1024]; //Buffer
+	char* path;
 	
 	//Fin de la Sección de Variables
 
 	for (;;){ //Bucle Infinito
-		printf("Shell ==> ");
+		if ((path = getcwd(NULL, 0)) == NULL){ //Obtenemos la ruta actual
+			
+			exit(5); //A partir de ahora el valor de error 5, se producira cuando no se pueda obtener la ruta actual
+		
+		} else {
+			
+			printf("\n~%s$ | Shell ==> ", path); //Mostramos el prompt con nuestra ruta actual
+			
+		}
 		if (fgets(buffer, 1024, stdin) == NULL){ //Leo la linea y si es igual a Null salgo del bucle
 			break;
 		} else {
 			line = tokenize(buffer); //Tokenizo, es decir, relleno la estructura de datos
+			if ((line == NULL) || (strlen(buffer) == 1)){ //Si la entrada estandar está vacia o su longitud es 1
+				continue; //Reinicia el bucle
+			}
 			printf("\n");
-			executeLine();
+			if (isChangeDirectory(line)){ //Si introducen cd
+				executeCd(line); //Ejecutar la función executeCd
+			} else {
+				
+				executeLine(); //En caso contrario ejecutar la función executeLine
+				
+			}
 		}
 		printf("\n");
 	}
